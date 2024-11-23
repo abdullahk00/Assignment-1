a5e550732211ce24149fa704417943e797416af6
abdullahk00 <abdullahkhan149.ak28@gmail.com>
2024-11-23 09:46:32 +0500

    Module B Assignment 1

---
+++ app.js
@@ -0,0 +1,24 @@
+//Assignment 1
+// Qns no 1
+
+let firstName = "abdullah";
+let age = 27;
+let occupation = "software Developer";
+let email = "abdullahkhan149.ak28@gail.com";
+let phoneNumber = 336 - 1224504;
+let education = "b com";
+
+document.write(
+  `my name is ${firstName} <br> my age is ${age} <br> my occupation is ${occupation} <br> my email is ${email} <br> my phone no is ${phoneNumber} <br> my education is ${education} <br>`
+);
+
+//Qns no 2
+
+console.log(x);
+var x = "Using var";
+let y = "Using let";
+const z = "Using const";
+
+console.log(x);
+console.log(y);
+console.log(z);
